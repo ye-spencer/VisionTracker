7c0a018c7305de6799f5e315d24ec3ab0fe5d16d
Spencer Ye <57966319+Concerned-Baby@users.noreply.github.com>
2023-01-17 18:44:53 -0800

    load in cv2

--- venv/main.py
+++ venv/main.py
@@ -1,9 +1,16 @@
 from process_timer import spencer_timer
+from camera_process import  camera_operator
 
 def main():
     timer = spencer_timer()
     timer.begin_timer("main")
+
+
     print("program begun")
+
+    camera = camera_operator()
+    camera.startscreen()
+
     timer.end_timer("main")
 
 

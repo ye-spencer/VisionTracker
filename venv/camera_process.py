f53d00df840a03039900beee1037ad9c1d496b85
Spencer Ye <57966319+ye-spencer@users.noreply.github.com>
2023-01-25 10:35:36 -0800

    add frames per second field

--- venv/camera_process.py
+++ venv/camera_process.py
@@ -1,5 +1,7 @@
 import cv2
 
+FRAMES_PER_SECOND = 30
+
 class camera_operator:
     def __init__(self):
         self.video = cv2.VideoCapture(0) #USB2.0 HD UVC WebCam
@@ -7,16 +9,11 @@ class camera_operator:
 
     def startscreen(self):
         while (True):
-            # ret, frame = self.video.read()
-            # cv2.imshow("frame", frame)
-            #
-            # if cv2.waitKey(1) & 0xFF == ord('q'):
-            #     break
             ret, frame = self.video.read()
+            cv2.imshow("Frame", frame)
 
-            cv2.imshow("kaleb is gay", frame)
+            cv2.waitKey(int(1000 / FRAMES_PER_SECOND))
 
-            cv2.waitKey(0)
 
         self.video.release()
 
